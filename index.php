cb91e23176004208c326531bae69c71a702c37b7
Bob Sforza <rpsforza@gmail.com>
2015-09-23 20:51:40 -0500

    a

--- index.php
+++ index.php
@@ -6,23 +6,21 @@
 		<meta name="description" content="MICDS Robotics Login">
 		<meta name="author" content="Michel Ge & Bob Sforza">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" rel="stylesheet">
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" rel="stylesheet">
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-		<link href='http://fonts.googleapis.com/css?family=Syncopate:700' rel='stylesheet' type='text/css'> -->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:600' rel='stylesheet' type='text/css'>
+		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script type="text/javascript" src="js/script.js"></script>
 		<script type="text/javascript" src="js/particles.js"></script>
 	</head>
 	<body>
+
 		<?php
-			if ($_POST["name"] != "") { 
-				echo "Your name is " . $_POST["name"];
-			}
+			$name = $_POST["name"];
+			$type = $_POST["inout"];
 		?>
+
 		<div id="wrap">
 		<div id="container">
 		<form action="http://micdsrobotics.herokuapp.com" method="post">
