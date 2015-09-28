44b300214830b32f86e0f6f703991b39b8b4ad7e
Bob Sforza <rpsforza@gmail.com>
2015-09-28 18:45:18 -0500

    NO SECURE

--- index.php
+++ index.php
@@ -116,7 +116,7 @@
 
 		<div id="wrap">
 		<div id="container">
-		<form action="https://micdsrobotics.herokuapp.com" method="post">
+		<form action="http://micdsrobotics.herokuapp.com" method="post">
 			<select id="name" name="name">
 				<option selected disabled value="Name">name</option>
 				<option value="Amir">Amir</option>
